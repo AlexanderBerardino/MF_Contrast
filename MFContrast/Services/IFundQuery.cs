21a7b3fc08fe10165e155e2e5d2e2b83ca5ce888
Alexander Berardino <alexander.berardino@student.fairfield.edu>
2020-04-07 17:25:39 -0400

    * IFundQuery.cs: Query Functionality abstracted
    * QueryUnitTest.cs: Testing Query results against previous
      implementations

---
+++ MFContrast/Services/IFundQuery.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MFContrast.Models;
+
+namespace MFContrast.Services
+{
+    public interface IFundQuery<T, H>
+    {
+        H RemoveNullHoldings(T holdings);
+        List<string> DistillUnion(T target, T except);
+        H DistillDifference(T target, T except);
+        double TopTen(T target);
+        double WeightedAverage(T target, List<string> union);
+        double COP(double x, double y);
+    }
+
+    public class ListFundQuery<T, H> : IFundQuery<T, H>
+        where T: IList<Holding>
+        where H: IEnumerable<Holding>
+    {
+        // Calculate Overlap Percentage
+        public double COP(double x, double y) => (x + y) / 2;
+
+        // Remove Holdings With Empty String or null For Symbol or Name
+        public H RemoveNullHoldings(T holdings)
+        {
+            var query = from T1 in holdings
+                        where !string.IsNullOrWhiteSpace(T1.Name)
+                        where !string.IsNullOrWhiteSpace(T1.Symbol)
+                        select T1;
+            return (H)query;
+        }
+
+        public double WeightedAverage(T d1, List<string> originolOtherList)
+        {
+            var query = from T1 in d1
+                        join T2 in originolOtherList on T1.Symbol equals T2
+                        select Convert.ToDouble(T1.Percentage);
+            return query.Sum();
+        }
+
+        public H DistillDifference(T targetList, T exceptList)
+        {
+            var query = exceptList.Select(X => X.Name);
+            var finalQuery = from T1 in targetList
+                             where !(query.Contains(T1.Name))
+                             select T1;
+            return (H)finalQuery;
+        }
+
+        public List<string> DistillUnion(T targetList, T exceptList)
+        {
+            var query = from T1 in targetList
+                        join T2 in exceptList on T1.Symbol equals T2.Symbol
+                        select T1.Symbol;
+            return query.ToList();
+        }
+
+        // Return sum of first ten holdings percentages 
+        public double TopTen(T targetList)
+        {
+            return targetList.Select(x => Convert.ToDouble(x.Percentage)).Take(10).Sum();
+        }
+    }
+}
---
+++ MFUnitTests/QueryUnitTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using MFContrast.Services;
+using MFContrast.Models;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+
+namespace MFUnitTests
+{
+    public class QueryUnitTest
+    {
+        public IFundQuery<IList<Holding>, IEnumerable<Holding>> Query { get; set; }
+        public DictionaryBasedCompare DictionaryBasedCompare { get; set; }
+
+        public MutualFund MockFundOne { get; set; }
+        public MutualFund MockFundTwo { get; set; }
+
+        public List<Holding> Holdings1 => MockFundOne.AssetList;
+        public List<Holding> Holdings2 => MockFundTwo.AssetList;
+
+        public List<Holding> CleanedHoldings1 { get; set; }
+        public List<Holding> CleanedHoldings2 { get; set; }
+
+        public QueryUnitTest()
+        {
+            Query = new ListFundQuery<IList<Holding>, IEnumerable<Holding>>();
+           
+
+            MockFundOne = new MutualFund("vfiax")
+            {
+                Id = "0",
+                FundName = "Vanguard",
+            };
+
+            MockFundTwo = new MutualFund("fcntx")
+            {
+                Id = "1",
+                FundName = "Fidelity",
+            };
+
+            CleanedHoldings1 = Query.RemoveNullHoldings(Holdings1).ToList();
+            CleanedHoldings2 = Query.RemoveNullHoldings(Holdings2).ToList();
+
+            DictionaryBasedCompare = new DictionaryBasedCompare(MockFundOne.AssetList, MockFundTwo.AssetList);
+        }
+
+        [Fact]
+        public void TestRemoveNullHoldings()
+        {
+            List<Holding> DBC = DictionaryBasedCompare.S1;
+            IEnumerable<Holding> queryEnumberable = Query.RemoveNullHoldings(Holdings1);
+            List<Holding> queryList = queryEnumberable.ToList();
+            Assert.Equal(DBC, queryList);
+        }
+
+        [Fact]
+        public void TestTopTen()
+        {
+            double DBC_Answer = DictionaryBasedCompare.F1TopTen;
+            double queryAnswer = Query.TopTen(Holdings1);
+            Assert.Equal(DBC_Answer, queryAnswer, 3);           
+        }
+
+        [Fact]
+        public void TestDistillUnion()
+        {
+            List<string> DBC = DictionaryBasedCompare.S1UnionS2;
+
+            IEnumerable<string> queryEnumberable = Query.DistillUnion(CleanedHoldings1, CleanedHoldings2);
+            List<string> queryToList = queryEnumberable.ToList();
+
+            Assert.Equal(DBC, queryToList);
+        }
+
+        [Fact]
+        public void TestDistillDifference()
+        {
+            List<Holding> DBC = DictionaryBasedCompare.S1Complement;
+            List<Holding> q = Query.DistillDifference(CleanedHoldings2, CleanedHoldings1).ToList();
+            Assert.Equal(DBC, q);
+        }
+
+        [Fact]
+        public void TestWeightedAverage()
+        {
+            List<string> queryUnion = Query.DistillUnion(CleanedHoldings1, CleanedHoldings2);
+            double q = Query.WeightedAverage(Holdings1, queryUnion);
+            double DBC = DictionaryBasedCompare.F2InF1;
+            Assert.Equal(DBC, q);
+        }
+    }
+}
